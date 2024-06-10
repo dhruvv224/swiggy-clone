188085f726845a10e3393191b6674d2d6a0fc5a0
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-10 22:47:59 +0530

    p tag

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -6,14 +6,23 @@ export const Category = () => {
   return (
     <div className='max-w-[1200px] mx-auto'>
       <div className='flex items-center justify-between my-7'>
-        <div className='text-[#282c3f] text-[40px] font-bold'>
+        <div>
+        <div className='text-[#282c3f] text-[40px] font-bold mx-11'>
           What's in your mind?
+         
         </div>
+
+        <p>Explore a wide variety of Food to satisfy your cravings.
+        </p>
+        </div>
+       
+        <br />
+        
         <div className='flex'>
           <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex items-center justify-center cursor-pointer'>
             <FaArrowLeft />
           </div>
-          <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex items-center justify-center'>
+          <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex items-center justify-center cursor-pointer'>
             <FaArrowRight />
           </div>
         </div>
