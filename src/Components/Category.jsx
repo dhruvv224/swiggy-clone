63fe250bee37bb9135655e56ce0e89dd2f2b01ca
dhruvv224 <123578746+dhruvv224@users.noreply.github.com>
2024-06-11 12:54:37 +0530

    commit

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,31 +1,62 @@
-import React from 'react';
-import { FaArrowRight } from "react-icons/fa";
-import { FaArrowLeft } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import Categorydata from './Data/Category.json';
 
 export const Category = () => {
+  const [Category, setCategory] = useState([]);
+  const[slide,setSlide]=useState(0)
+
+  const FetchData = async () => {
+    setCategory(Categorydata);
+    console.log(Categorydata);
+  };
+
+  useEffect(() => {
+    FetchData();
+  }, []);
+  const leftSlide=()=>{
+    if(slide==0)
+      return false
+    setSlide(slide - 3)
+  }
+const rightSlide=()=>{
+  if(slide==18)
+    return false
+ 
+ 
+
+  setSlide(slide + 3)
+}
   return (
     <div className='max-w-[1200px] mx-auto'>
       <div className='flex items-center justify-between my-7'>
         <div>
-        <div className='text-[#282c3f] text-[40px] font-bold mx-11'>
-          What's in your mind?
-         
+          <div className='text-[#282c3f] text-[40px] font-bold mx-11'>
+            What's on your mind?
+          </div>
+          <p className='text-[#282c3f] text-[16px]'>
+            Explore a wide variety of Food to satisfy your cravings.
+          </p>
         </div>
-
-        <p className='text-[#282c3f] text-[16px]'>Explore a wide variety of Food to satisfy your cravings.
-        </p>
-        </div>
-       
         <br />
-        
         <div className='flex'>
           <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex items-center justify-center cursor-pointer'>
-            <FaArrowLeft />
+            <FaArrowLeft onClick={leftSlide}/>
           </div>
           <div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex items-center justify-center cursor-pointer'>
-            <FaArrowRight />
+            <FaArrowRight onClick={rightSlide} />
           </div>
         </div>
+      </div>
+      <div className='flex overflow-hidden'>
+        {Category.map((item, index) => (
+        
+          <div key={index} style={{transform:`translateX(-${slide *100}%)`}} className='w-[150px] m-4 text-center shrink-0 duration-500'>
+            <img src={item.image} alt={item.path} className='w-full h-auto' />
+            <h1 className='text-[#282c3f] text-[18px] font-medium'>{item.path}</h1>
+          </div>
+         
+        ))}
       </div>
     </div>
   );
